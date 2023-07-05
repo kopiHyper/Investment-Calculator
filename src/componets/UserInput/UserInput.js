afadae7b1e7806720a012ec56b8238040e92b4d9
kopiHyper <oktawiankopica2001@gmail.com>
2023-07-05 18:32:03 +0200

    Handling user inputs and managing state

--- src/componets/UserInput/UserInput.js
+++ src/componets/UserInput/UserInput.js
@@ -1,21 +1,35 @@
 import { useState } from "react";
 import "./UserInput.css";
 
+const initialUserInput = {
+  "current-savings": 0,
+  "yearly-contribution": 0,
+  "expected-return": 0,
+  duration: 0,
+};
+
 const UserInput = () => {
-  // const [currentSavings, setCurrentSavings] = useState("");
-  // const [yearlyContribution, setYearlyContribution] = useState("");
-  // const [expectedReturn, setExpectedReturn] = useState("");
-  // const [duration, setDuration] = useState("");
+  const [userInput, setUserInput] = useState(initialUserInput);
 
-  // const handleInputsChange = (event) => {
-  //   setCurrentSavings(event.target.value);
-  // };
+  const handleInputChange = (input, value) => {
+    setUserInput((prevState) => {
+      return {
+        ...prevState,
+        [input]: value,
+      };
+    });
+  };
 
-  // const handleSubmit = (event) => {
-  //   event.preventDefault();
-  //   // Handle form submission logic
-  //   console.log(currentSavings);
-  // };
+  const resetHandler = () => {
+    setUserInput(initialUserInput);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // Handle form submission logic
+    console.log(userInput);
+  };
+
   const calculateHandler = (userInput) => {
     // Should be triggered when form is submitted
     // You might not directly want to bind it to the submit event on the form though...
@@ -44,15 +58,29 @@ const UserInput = () => {
     // do something with yearlyData ...
   };
   return (
-    <form className="form">
+    <form className="form" onSubmit={handleSubmit}>
       <div className="input-group">
         <p>
           <label htmlFor="current-savings">Current Savings ($)</label>
-          <input type="number" id="current-savings" />
+          <input
+            type="number"
+            id="current-savings"
+            value={userInput["current-savings"]}
+            onChange={(event) =>
+              handleInputChange("current-savings", event.target.value)
+            }
+          />
         </p>
         <p>
           <label htmlFor="yearly-contribution">Yearly Savings ($)</label>
-          <input type="number" id="yearly-contribution" />
+          <input
+            type="number"
+            id="yearly-contribution"
+            value={userInput["yearly-contribution"]}
+            onChange={(event) =>
+              handleInputChange("yearly-contribution", event.target.value)
+            }
+          />
         </p>
       </div>
       <div className="input-group">
@@ -60,15 +88,29 @@ const UserInput = () => {
           <label htmlFor="expected-return">
             Expected Interest (%, per year)
           </label>
-          <input type="number" id="expected-return" />
+          <input
+            type="number"
+            id="expected-return"
+            value={userInput["expected-return"]}
+            onChange={(event) =>
+              handleInputChange("expected-return", event.target.value)
+            }
+          />
         </p>
         <p>
           <label htmlFor="duration">Investment Duration (years)</label>
-          <input type="number" id="duration" />
+          <input
+            type="number"
+            id="duration"
+            value={userInput["duration"]}
+            onChange={(event) =>
+              handleInputChange("duration", event.target.value)
+            }
+          />
         </p>
       </div>
       <p className="actions">
-        <button type="reset" className="buttonAlt">
+        <button type="reset" className="buttonAlt" onClick={resetHandler}>
           Reset
         </button>
         <button type="submit" className="button">
