8f1e0c333158b6b45d7399359162e6a87a0bd3fd
kopiHyper <oktawiankopica2001@gmail.com>
2023-07-08 22:37:17 +0200

    Moved out the' calculateHandler'

--- src/componets/UserInput/UserInput.js
+++ src/componets/UserInput/UserInput.js
@@ -8,7 +8,7 @@ const initialUserInput = {
   duration: 0,
 };
 
-const UserInput = () => {
+const UserInput = (props) => {
   const [userInput, setUserInput] = useState(initialUserInput);
 
   const handleInputChange = (input, value) => {
@@ -27,36 +27,9 @@ const UserInput = () => {
   const handleSubmit = (event) => {
     event.preventDefault();
     // Handle form submission logic
-    console.log(userInput);
+    props.onCalculate(userInput);
   };
 
-  const calculateHandler = (userInput) => {
-    // Should be triggered when form is submitted
-    // You might not directly want to bind it to the submit event on the form though...
-
-    const yearlyData = []; // per-year results
-
-    let currentSavings = +userInput["current-savings"]; // feel free to change the shape of this input object!
-    const yearlyContribution = +userInput["yearly-contribution"]; // as mentioned: feel free to change the shape...
-    const expectedReturn = +userInput["expected-return"] / 100;
-    const duration = +userInput["duration"];
-
-    // The below code calculates yearly results (total savings, interest etc)
-    for (let i = 0; i < duration; i++) {
-      const yearlyInterest = currentSavings * expectedReturn;
-      currentSavings += yearlyInterest + yearlyContribution;
-      yearlyData.push({
-        // feel free to change the shape of the data pushed to the array!
-        year: i + 1,
-        yearlyInterest: yearlyInterest,
-        savingsEndOfYear: currentSavings,
-        yearlyContribution: yearlyContribution,
-      });
-    }
-    console.log(currentSavings);
-
-    // do something with yearlyData ...
-  };
   return (
     <form className="form" onSubmit={handleSubmit}>
       <div className="input-group">
