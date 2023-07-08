16a60e128fe698b8729c6b82c96a4ee3efd15a2a
kopiHyper <oktawiankopica2001@gmail.com>
2023-07-08 22:35:45 +0200

    Outputting results conditionally

--- src/App.js
+++ src/App.js
@@ -1,18 +1,45 @@
+import { useState } from "react";
 import Header from "./componets/Header/Header";
 import ResultsTable from "./componets/ResultsTable/ResultsTable";
 import UserInput from "./componets/UserInput/UserInput";
 
 function App() {
+  const [userInput, setUserInput] = useState(null);
+
+  const calculateHandler = (userInput) => {
+    setUserInput(userInput);
+  };
+
+  if (userInput) {
+    const yearlyData = [];
+
+    let currentSavings = +userInput["current-savings"];
+    const yearlyContribution = +userInput["yearly-contribution"];
+    const expectedReturn = +userInput["expected-return"] / 100;
+    const duration = +userInput["duration"];
+
+    // The below code calculates yearly results (total savings, interest etc)
+    for (let i = 0; i < duration; i++) {
+      const yearlyInterest = currentSavings * expectedReturn;
+      currentSavings += yearlyInterest + yearlyContribution;
+      yearlyData.push({
+        // feel free to change the shape of the data pushed to the array!
+        year: i + 1,
+        yearlyInterest: yearlyInterest,
+        savingsEndOfYear: currentSavings,
+        yearlyContribution: yearlyContribution,
+      });
+    }
+  }
+
   return (
     <div>
       <Header />
 
-      <UserInput />
+      <UserInput onCalculate={calculateHandler} />
 
-      {/* Todo: Show below table conditionally (only once result data is available) */}
-      {/* Show fallback text if no data is available */}
-
-      <ResultsTable />
+      {!userInput && <p>No investmentrs calculated yet</p>}
+      {userInput && <ResultsTable />}
     </div>
   );
 }
